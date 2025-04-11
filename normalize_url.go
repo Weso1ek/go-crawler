830b9ad34dc11bacef3f12114272340da1a1337d
Sebastian Wesołowski <Sebastian.Wesolowski@grupawp.pl>
2025-04-11 21:47:22 +0200

    Get urls from html

--- normalize_url.go
+++ normalize_url.go
@@ -1,17 +1,63 @@
 package main
 
 import (
-	"fmt"
+	"golang.org/x/net/html"
 	"net/url"
+	"strings"
 )
 
 func normalizeURL(inputURL string) (string, error) {
-	fmt.Println("Hello, World!")
-
 	u, err := url.Parse(inputURL)
 	if err != nil {
 		panic(err)
 	}
-	
+
 	return u.Host + u.Path, nil
+}
+
+func getURLsFomHTML(htmlBody, rawBaseURL string) ([]string, error) {
+
+	var urls []string
+	var urlsParsed []string
+
+	reader := strings.NewReader(htmlBody)
+
+	parsed, err := html.Parse(reader)
+	if err != nil {
+		return urls, err
+	}
+
+	processLinks(parsed, &urlsParsed)
+
+	for _, j := range urlsParsed {
+		u, err := url.Parse(j)
+
+		if err != nil {
+			return urls, err
+		}
+
+		if u.Host == "" {
+			urls = append(urls, rawBaseURL+j)
+		}
+
+		if u.Host != rawBaseURL && u.Host != "" {
+			urls = append(urls, j)
+		}
+	}
+
+	return urls, nil
+}
+
+func processLinks(n *html.Node, urls *[]string) {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				*urls = append(*urls, a.Val)
+			}
+		}
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		processLinks(c, urls)
+	}
 }
---
+++ normalize_url_bootdev.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/html"
+	"net/url"
+	"strings"
+)
+
+func normalizeURLBootdev(rawURL string) (string, error) {
+	parsedURL, err := url.Parse(rawURL)
+	if err != nil {
+		return "", fmt.Errorf("couldn't parse URL: %w", err)
+	}
+
+	fullPath := parsedURL.Host + parsedURL.Path
+
+	fullPath = strings.ToLower(fullPath)
+
+	fullPath = strings.TrimSuffix(fullPath, "/")
+
+	return fullPath, nil
+}
+
+func getURLsFromHTMLBootdev(htmlBody, rawBaseURL string) ([]string, error) {
+	baseURL, err := url.Parse(rawBaseURL)
+	if err != nil {
+		return nil, fmt.Errorf("couldn't parse base URL: %v", err)
+	}
+
+	htmlReader := strings.NewReader(htmlBody)
+	doc, err := html.Parse(htmlReader)
+	if err != nil {
+		return nil, fmt.Errorf("couldn't parse HTML: %v", err)
+	}
+
+	var urls []string
+	var traverseNodes func(*html.Node)
+	traverseNodes = func(node *html.Node) {
+		if node.Type == html.ElementNode && node.Data == "a" {
+			for _, anchor := range node.Attr {
+				if anchor.Key == "href" {
+					href, err := url.Parse(anchor.Val)
+					if err != nil {
+						fmt.Printf("couldn't parse href '%v': %v\n", anchor.Val, err)
+						continue
+					}
+
+					resolvedURL := baseURL.ResolveReference(href)
+					urls = append(urls, resolvedURL.String())
+				}
+			}
+		}
+
+		for child := node.FirstChild; child != nil; child = child.NextSibling {
+			traverseNodes(child)
+		}
+	}
+	traverseNodes(doc)
+
+	return urls, nil
+}
