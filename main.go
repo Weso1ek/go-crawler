ae52c7cda990e6fd678d6dcff53f2cbd3bef7ff4
Sebastian Wesołowski <Sebastian.Wesolowski@grupawp.pl>
2025-04-11 21:59:01 +0200

    Command line args

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
 
+	args := os.Args[1:]
+
+	if len(args) < 1 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	}
+
+	if len(args) > 1 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+
+	fmt.Println("starting crawl of: ", os.Args[1])
 }
